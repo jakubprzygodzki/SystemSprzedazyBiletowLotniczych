c1d201e3ff8b5b539397a9b1d6ebfadb9bb8ea71
Kuba <jakub.przygodzki@student.wat.edu.pl>
2018-12-20 01:55:40 +0100

    Dodano rejestracje

---
+++ src/RegisterData.java
@@ -0,0 +1,8 @@
+
+public class RegisterData {
+	
+	public String login;
+	public String email;
+	public String password;
+
+}
---
+++ src/RegisterServlet.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import art.copy.Article;
+
+public class RegisterServlet extends HttpServlet{
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+		String json = "";
+
+		if (br != null) {
+			json = br.readLine();
+		}
+		System.out.println(json);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		RegisterData register = mapper.readValue(json, RegisterData.class);
+		
+		
+		
+		
+		// size of byte buffer to send file
+		final int BUFFER_SIZE = 414096;
+
+		// database connection settings
+		String dbURL = "jdbc:mysql://localhost:3306/flights";
+		String dbUser = "root";
+		String dbPass = "";
+
+		Connection conn=null; // connection to the database
+
+		//String req = request.getParameter("fromCity");
+			System.out.println("przerzucam do post");
+			System.out.println("request:" + register);
+
+			String sql = null;
+			java.sql.Statement stmt = null;
+			java.sql.Statement stmt2 = null;
+
+			try {
+				Driver sterownik = new com.mysql.jdbc.Driver();
+				DriverManager.registerDriver(sterownik);
+				Connection conn1 = sterownik.connect("jdbc:mysql://localhost:3306/flights?user=root&password=", null);
+		
+				stmt = conn1.createStatement();
+				stmt2 = conn1.createStatement();
+
+				String haslo = "";
+				System.out.println("start select register");
+				if (stmt.execute("Select login, email, password from uzytkownicy where login = '" + register.login + "'")) {
+
+					ResultSet zbior = stmt.getResultSet();
+
+					if(zbior.next()) {
+					
+						mapper.writeValue(response.getOutputStream(), "Uzytkownik o podanym loginie już istnieje!");
+					}
+					else {
+						 
+						boolean bStatement2 = stmt2.execute("INSERT INTO uzytkownicy (login, email, password) VALUES (" + "'" + register.login + "'"  + ", " + "'" + register.email+ "'"  + ", " + "'" + register.password + "'" + ")");
+						mapper.writeValue(response.getOutputStream(), "Zarejestrowano!");
+					}
+					System.out.println("pooo");
+				}
+
+			} catch (Exception e) {
+				System.out.println(e);
+			} finally {
+				try {
+					stmt.close();
+					stmt2.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			//return null;
+
+		
+		
+	}
+}
